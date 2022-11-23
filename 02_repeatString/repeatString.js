2f7d916c069de345bf4f5ea612aeda4dd1750dcd
kayaba <Stefan.siggaard.andersen@gmail.com>
2022-11-23 22:20:45 +0100

    Completed 01 and 02 tutorials

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, num) {
+
+    if(num<0){
+        return "ERROR";
+    }
+
+    let stringToReturn = "";
+    for(let i=0; i<num; i++){
+        stringToReturn += stringToRepeat;
+    }
+    return stringToReturn;
 
 };
 
