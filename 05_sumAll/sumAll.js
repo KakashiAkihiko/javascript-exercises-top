ca2360218bfdc212e47a6e56118f09c58184880b
kayaba <Stefan.siggaard.andersen@gmail.com>
2022-11-24 11:05:45 +0100

    Finished 05 sum all Checked for valid inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(numFloor, numCeiling) {
+
+
+    //Check if Integer
+    if( (!Number.isInteger(numFloor)) || (!Number.isInteger(numCeiling)) ){
+        return "ERROR";
+    }
+
+    //Check if input is negative
+    if(numFloor<0 || numCeiling<0){
+        return "ERROR";
+    }
+
+    //Check input is in order of magnitude
+    if(numCeiling<numFloor){
+        let temp = numCeiling;
+        numCeiling = numFloor;
+        numFloor = temp;
+    }
+
+    let result = 0;
+    //Iterate from the bottom number to top number adding the next num to current result every iteration
+    for(let i=numFloor; i<numCeiling+1; i++){
+        result += i;
+    }
+    return result;
 
 };
 
