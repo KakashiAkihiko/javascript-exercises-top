d644de2a146724ced6369e52d635ef3fac81509b
kayaba <Stefan.siggaard.andersen@gmail.com>
2022-11-24 11:44:08 +0100

    Solved 07 - No issues

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahren) {
+
+  return Math.round(((tempFahren - 32) * 5/9)*10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+
+  return (Math.round(((tempCel * (9/5) +32))*10) / 10);
+
 };
 
 // Do not edit below this line
@@ -9,3 +15,10 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+
+/*
+how to round
+var rounded = Math.round(number * 10) / 10
+*/
