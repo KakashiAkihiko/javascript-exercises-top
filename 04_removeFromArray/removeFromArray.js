9e413f4bf243a620dedf3047958e9b4c04512b9d
kayaba <Stefan.siggaard.andersen@gmail.com>
2022-11-24 10:39:26 +0100

    Finished 04 - delete specific array elements Mastered calling functions with more arguments and parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, numToRemove) {
+
+    for (let j = 0; j < arguments.length; j++) {
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arguments[j]) {
+                array.splice(i, 1)
+            }
+        }
+    }
+
+    return array;
 
 };
 
