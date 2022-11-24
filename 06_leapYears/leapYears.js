8de687f502cc99074b053f57e21d3b3a342d4f4c
kayaba <Stefan.siggaard.andersen@gmail.com>
2022-11-24 11:13:35 +0100

    Exercise 06 completed That was extremely easy making use of the remainder

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(numIsLeap) {
+
+    if(numIsLeap % 4 === 0){
+        if(numIsLeap % 100 !== 0){
+            return true;
+        }else if(numIsLeap % 400 === 0){
+            return true;
+        }
+    }
+    return false;
 
 };
 
