04f1f52889109a8134c87133d3fe593601544623
kayaba <Stefan.siggaard.andersen@gmail.com>
2022-11-24 12:14:15 +0100

    08 completed. Created a calculator Neat

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,57 @@
-const add = function() {
+const add = function(num1, num2) {
+  
+  return num1+num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+
+  return num1-num2;
 	
 };
 
-const sum = function() {
+const sum = function(numArray) {
+  let result = 0;
+  
+  for(let i = 0; i<numArray.length; i++){
+    result += numArray[i];
+  }
+  return result;
 	
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+
+  if(numArray.length === 0){
+    return 0;
+  }
+
+  let result = 1;
+  for(let i = 0; i<numArray.length; i++){
+    result *= numArray[i];
+  }
+  return result;
 
 };
 
-const power = function() {
+const power = function(num1, num2) {
+
+  return Math.pow(num1, num2);
 	
 };
 
-const factorial = function() {
+const factorial = function(numToFactorial) {
+
+  if(numToFactorial === 0 || numToFactorial === 1){
+    return 1;
+  }
+
+  let result = 1;
+
+  for(let i = 1; i<numToFactorial+1; i++){
+    result *= i;
+  }
+  return result;
 	
 };
 
